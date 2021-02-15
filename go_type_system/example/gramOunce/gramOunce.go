b9a3af288cd485c493b805f86af140942d4b6956
rahulsidpatil <rahulsidgondapatil@gmail.com>
2021-02-15 13:42:33 +0530

    improve examples

--- go_type_system/example/gramOunce/gramOunce.go
+++ go_type_system/example/gramOunce/gramOunce.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // gram => defined/named type; float64 => source type; float64 => underlying type
 type gram float64
@@ -11,16 +13,17 @@ type ounce float64
 type gm = float64
 type on = float64
 
+const mfactor = 0.035274
+
 func main() {
-	// How it works with the Defined types
 	var g gram = 1000
 	var o ounce
-	o = ounce(g) * 0.035274 // a Defined type inherits underlying type's operations
+	o = ounce(g) * mfactor // a Defined type inherits underlying type's operations
 	fmt.Printf("%g grams is: %.2f ounces\n", g, o)
 
 	// How it works with the Aliased types
 	var gr gm = 1000
 	var ou on
-	ou = gr * 0.035274 // an Aliased type is same as it's source type
+	ou = gr * mfactor // an Aliased type is same as it's source type
 	fmt.Printf("%g gm is: %.2f ou\n", gr, ou)
 }
