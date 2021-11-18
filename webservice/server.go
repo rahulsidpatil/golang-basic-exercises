d1c375e559d6b136242534b5626750d8da1027d4
rahulsidpatil <rahulsidgondapatil@gmail.com>
2021-11-18 08:43:44 +0530

    craete a raw restful web service in go

--- webservice/server.go
+++ webservice/server.go
@@ -1,31 +1,91 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
-type helloHandler struct {
-	Msg string
+const (
+	WELCOME_MSG     = "Welcome to the online store!!!"
+	STORE_EMPTY_MSG = "The store is empty. try later!!"
+)
+
+type Product struct {
+	Name string
+	ID   int
 }
 
-func (h *helloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(h.Msg))
+type Store struct {
+	Products []Product
 }
 
-func middlewarefunc(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		//actual middleware code goes here
-		w.Write([]byte("middleware called!!\n"))
-		handler.ServeHTTP(w, r)
-	})
+func (s *Store) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		if len(s.Products) == 0 {
+			rw.Write([]byte(STORE_EMPTY_MSG))
+			return
+		} else {
+			data, err := json.Marshal(s)
+			if err != nil {
+				rw.WriteHeader(http.StatusInternalServerError)
+				rw.Write([]byte(err.Error()))
+				return
+			}
+			rw.Write([]byte(data))
+		}
+	case http.MethodPost:
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			rw.WriteHeader(http.StatusBadRequest)
+			rw.Write([]byte(err.Error()))
+			return
+		}
+		newProduct := Product{}
+		err = json.Unmarshal(data, &newProduct)
+		if err != nil {
+			rw.WriteHeader(http.StatusBadRequest)
+			rw.Write([]byte(err.Error()))
+			return
+		}
+		for _, p := range s.Products {
+			if newProduct.ID == p.ID {
+				rw.WriteHeader(http.StatusConflict)
+				return
+			}
+		}
+		s.Products = append(s.Products, newProduct)
+		rw.WriteHeader(http.StatusCreated)
+
+	case http.MethodDelete:
+		pathParams := strings.Split(r.URL.Path, "products/")
+		pid, err := strconv.Atoi(pathParams[len(pathParams)-1])
+		if err != nil {
+			rw.WriteHeader(http.StatusBadRequest)
+			rw.Write([]byte(err.Error()))
+			return
+		}
+		for i, p := range s.Products {
+			if pid == p.ID {
+				items := s.Products[:i]
+				s.Products = append(items, s.Products[i+1:]...)
+				rw.WriteHeader(http.StatusOK)
+				return
+			}
+		}
+	}
 }
 
 func main() {
-	h := &helloHandler{Msg: "Hello client!!"}
-	http.HandleFunc("/svc", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("webservice called using handleFUnc!!"))
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write([]byte(WELCOME_MSG))
 	})
-	http.Handle("/hello", middlewarefunc(h))
+	s := &Store{Products: []Product{{"soap", 1}}}
+	http.Handle("/store/products", s)
+	http.Handle("/store/products/", s)
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
